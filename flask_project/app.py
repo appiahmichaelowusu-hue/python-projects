b2d5b0c17ac0c696d3d12dbf0ca8bdda077e19ac
unknown <appiahmichaelowusu@gmail.com>
2026-06-20 19:13:37 +0000

    Added a flask project

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,7 +1,17 @@
 # app.py
 from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+
 
 @app.route("/form")
 def form():
@@ -10,7 +20,15 @@ def form():
 @app.route("/greet", methods=["POST"])
 def greet():
     name = request.form["username"]
-    return render_template("greeting.html", name=name)
+    phone = str(request.form["phone"])
+    new_contact = Contact(name=name, phone=phone)
+    db.session.add(new_contact)
+    db.session.commit()
+    return render_template("greeting.html", name=name, phone=phone)
+   
+
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
